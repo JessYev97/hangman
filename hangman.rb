e2b771159ad96c5760ea4c6d2c1aaacc975d3f42
JessYev97 <jessmyeverino@gmail.com>
2025-03-17 13:07:15 -0500

    add stick figure
    writes logic for what the program prints when incorrect guesses are made

--- hangman.rb
+++ hangman.rb
@@ -6,8 +6,17 @@ class Hangman
     @dictionary = dictionary
     @secret_word = ""
     @correct_length_words = []
+    @head = ""
+    $arms = "" 
+    @right_arm = ""
+    @left_arm = "" 
+    @torso = "" 
+    $legs = "" 
+    @right_leg = ""
+    @left_leg = ""
+    
     $word_place_holder = [] # may end up as a string
-    $incorrect_guesses_array = [] 
+    $incorrect_guesses_array = []
   end
 
   def select_desired_words
@@ -43,16 +52,76 @@ class Hangman
           $word_place_holder[letter_index] = letter
         end
       end
-    else 
-      route_incorrect_guess 
-    end 
-    puts $word_place_holder.join(" ") 
+      puts $word_place_holder.join(" ")
+    else
+      route_incorrect_guess
+    end
+
+    check_game_status
   end
 
   def route_incorrect_guess
-    $incorrect_guesses_array << $letter_guess 
+    $incorrect_guesses_array << $letter_guess
     puts "#{$letter_guess} is an incorrect guess"
-    puts "The list of incorrect letters so far is: #{$incorrect_guesses_array}" 
+    puts "The list of incorrect letters so far is: #{$incorrect_guesses_array.join(", ")}"
+  end
+
+  def check_game_status
+    if $incorrect_guesses_array.length >= 6
+      @head = " (x_x)"
+      puts_body
+      puts "The whole man is hung! You lost."
+    elsif $word_place_holder.join("") == @secret_word
+      puts "Well done! You won."
+    else
+      display_hanged_man
+      puts_body
+      guess_a_letter
+    end
+  end
+
+  def display_hanged_man
+    case $incorrect_guesses_array.length
+    when 0
+      puts "safe so far..."
+    when 1
+      @torso = "|"
+    when 2
+      @torso = "|"
+      @left_leg = "/"
+    when 3
+      @torso = "|"
+      @left_leg = "/" 
+      @right_leg = "\\" 
+    when 4
+      @torso = "|"
+      @left_leg = "/" 
+      @right_leg = "\\" 
+      @right_arm = "\\"
+    when 5
+      @torso = "|"
+      @left_leg = "/" 
+      @right_leg = "\\" 
+      @right_arm = "\\" 
+      @left_arm = "/" 
+      puts "Watch out! He'll be hanged by his neck if you make another mistake!"
+    when 6 
+      @torso = "   |   "
+      @left_leg = "      /" 
+      @right_leg = "\\      " 
+      @right_arm = "      /" 
+      @left_arm = "      \\" 
+      @head = " (x_x) "
+    end
+  end
+
+  def puts_body
+    puts @head 
+    $arms = "#{@right_arm} #{@left_arm}"
+    puts $arms 
+    puts @torso
+    $legs = "#{@right_leg} #{@left_leg}"
+    puts $legs
   end 
 
   def begin_game
